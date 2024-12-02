5557464a8bf431c449a6d27d4a0b1cff4dc4283f
Mustakim-Taki <abrar.mustakim@bjitacademy.com>
2024-12-02 15:32:20 +0600

    Commiting before pushing

--- mytodolist/urls.py
+++ mytodolist/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from reviews import views  # Import your app's views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
